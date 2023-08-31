8df1602bad2c99e741653c26f5a1d085a68fb492
viktor-dimitrov <106183268+viktor-dimitrov@users.noreply.github.com>
2023-08-31 20:15:37 +0300

    1

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -8,22 +8,9 @@ function App() {
 
   return (
     <>
-      <div>
- 
 
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>My First Vite Project</h1>
+
     </>
   )
 }
